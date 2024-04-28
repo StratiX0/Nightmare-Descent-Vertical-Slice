df9fc057b5b8ffa40b91efae5aeb20ede2a9ca0c
StratiX0 <jerome.hussenet@sfr.fr>
2024-04-28 20:06:17 +0200

    feat: Size and position when creating gameObject

--- Game/Scenes/DefaultScene.h
+++ Game/Scenes/DefaultScene.h
@@ -11,25 +11,25 @@ class DefaultScene final : public Scene
 public:
 	DefaultScene() : Scene("DefaultScene")
 	{
-		GameObject* player = CreateDummyGameObject("Player", 200.f, sf::Color::Red);
+		GameObject* player = CreaInGameObject("Player", Maths::Vector2f(200.f,200.0f), Maths::Vector2f(20.f, 20.0f), sf::Color::Red);
 		player->CreateComponent<Player>();
 
-		GameObject* enemy = CreateDummyGameObject("Enemy", 400.f, sf::Color::Blue);
-		GameObject* enemy2 = CreateDummyGameObject("Enemy2", 0.f, sf::Color::Green);
+		GameObject* enemy = CreaInGameObject("Enemy", Maths::Vector2f(400.f, 400.0f), Maths::Vector2f(10.f, 10.0f), sf::Color::Blue);
+		GameObject* enemy2 = CreaInGameObject("Enemy2", Maths::Vector2f(0.f, 0.0f), Maths::Vector2f(50.f, 50.0f), sf::Color::Green);
 	}
 
-	GameObject* CreateDummyGameObject(const std::string& _name, const float _position, const sf::Color _color)
+	GameObject* CreaInGameObject(const std::string& _name, Maths::Vector2f _position, Maths::Vector2f _size, const sf::Color _color)
 	{
 		GameObject* game_object = CreateGameObject(_name);
-		game_object->SetPosition(Maths::Vector2f(_position, _position));
+		game_object->SetPosition(Maths::Vector2f(_position));
 
 		SquareCollider* square_collider = game_object->CreateComponent<SquareCollider>();
-		square_collider->SetWidth(20.f);
-		square_collider->SetHeight(20.f);
+		square_collider->SetWidth(_size.x);
+		square_collider->SetHeight(_size.y);
 
 		RectangleShapeRenderer* shape_renderer = game_object->CreateComponent<RectangleShapeRenderer>();
 		shape_renderer->SetColor(_color);
-		shape_renderer->SetSize(Maths::Vector2f(200.f, 200.f));
+		shape_renderer->SetSize(_size);
 
 		return game_object;
 	}
