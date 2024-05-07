b16f77461cc8a43ff978976552a3d95171a552b2
StratiX0 <jerome.hussenet@sfr.fr>
2024-05-07 20:35:50 +0200

    docs: +Commentaires AnimatedSprite

--- SFMLEngine/include/AnimatedSprite.h
+++ SFMLEngine/include/AnimatedSprite.h
@@ -2,10 +2,12 @@
 #include "Component.h"
 #include <SFML/Graphics.hpp>
 
+// La classe AnimatedSpriteComponent herite de la classe Component et represente un sprite anime dans le jeu.
 class AnimatedSpriteComponent : public Component
 {
 public:
 
+    // Enumeration representant les directions de mouvement possibles.
     enum MovementDirection
     {
         Left,
@@ -14,38 +16,59 @@ public:
         Down
     };
 
+    // Enumeration representant les etats possibles du sprite du joueur.
     enum class PlayerSpriteState
     {
-        Idle = 15,      // 15 frames for Idle
-        Running = 8,   // 8 frames for Running
+        Idle,
+        Running
     };
 
-
+    // Constructeur et destructeur.
     AnimatedSpriteComponent();
     ~AnimatedSpriteComponent() override;
 
+    // Methodes pour gerer la texture du sprite.
     void SetTexture(sf::Texture* _texture);
     void LoadTexture(const std::string& _texturePath);
+
+    // Methodes pour gerer le nombre de frames de l'animation.
     void SetFrameCount(int _frameCount) { frameCount = _frameCount; }
+    int GetFrameCount() const { return frameCount; }
+
+    // Methodes pour acceder au sprite et a la texture.
     sf::Sprite* GetSprite() { return sprite; }
     sf::Texture* GetTexture() { return texture; }
-    int GetFrameCount() const { return frameCount; }
-    void SetFrameTime(float _frameTime) { frameTime = _frameTime; }
-    void SetCurrentFrame(int _currentFrame) { currentFrame = _currentFrame; }
-    void SetCurrentTime(float _currentTime) { currentTime = _currentTime; }
-    void SetTextureRect(int _left, int _top, int _width, int _height);
-    void SetDirection(MovementDirection _direction);
-	MovementDirection GetDirection() const { return direction; }
-	void SetDefaultScale(float _scaleX, float _scaleY) { defaultScaleX = _scaleX; defaultScaleY = _scaleY; }
 
+    // Methodes pour gerer le temps entre les frames.
+    void SetFrameTime(float _frameTime) { frameTime = _frameTime; }
+
+    // Methodes pour gérer la frame actuelle.
+    void SetCurrentFrame(int _currentFrame) { currentFrame = _currentFrame; }
+
+    // Methodes pour gerer le temps ecoule depuis le debut de l'animation.
+    void SetCurrentTime(float _currentTime) { currentTime = _currentTime; }
+
+    // Methode pour definir le rectangle de texture du sprite.
+    void SetTextureRect(int _left, int _top, int _width, int _height);
+
+    // Methodes pour gerer la direction du mouvement.
+    void SetDirection(MovementDirection _direction);
+    MovementDirection GetDirection() const { return direction; }
+
+    // Methodes pour gerer l'echelle par defaut du sprite.
+    void SetDefaultScale(float _scaleX, float _scaleY) { defaultScaleX = _scaleX; defaultScaleY = _scaleY; }
+
+    // Methodes pour mettre a jour et dessiner le sprite.
     void Update(float deltaTime) override;
     void Render(sf::RenderWindow* window) override;
 
+    // Variables representant la direction et l'etat actuels du sprite.
     MovementDirection direction;
     PlayerSpriteState state;
     PlayerSpriteState lastState;
 
-	void SetState(PlayerSpriteState _state) { state = _state; }
+    // Methodes pour gerer l'etat du sprite.
+    void SetState(PlayerSpriteState _state) { state = _state; }
     void SetStateFrameCount(PlayerSpriteState state, int frameCount) { stateFrameCounts[state] = frameCount; SetFrameCount(frameCount); }
     int GetStateFrameCount(PlayerSpriteState state) const;
     void SetStateFilePath(PlayerSpriteState state, const std::string& filePath) { stateFilePaths[state] = filePath; }
@@ -53,15 +76,21 @@ public:
 
 private:
 
+    // Maps pour stocker le nombre de frames et le chemin du fichier pour chaque etat.
     std::map<PlayerSpriteState, int> stateFrameCounts;
     std::map<PlayerSpriteState, std::string> stateFilePaths;
+
+    // Pointeur vers le sprite et la texture.
     sf::Sprite* sprite;
     sf::Texture* texture;
+
+    // Variables pour stocker le nombre de frames, le temps entre les frames, la frame actuelle et le temps ecoule depuis le debut de l'animation.
     int frameCount;
     float frameTime;
     int currentFrame;
     float currentTime;
-	float defaultOriginX, defaultOriginY;
-	float defaultScaleX, defaultScaleY;
 
+    // Variables pour stocker l'origine et l'echelle par defaut du sprite.
+    float defaultOriginX, defaultOriginY;
+    float defaultScaleX, defaultScaleY;
 };
--- SFMLEngine/src/AnimatedSprite.cpp
+++ SFMLEngine/src/AnimatedSprite.cpp
@@ -1,6 +1,8 @@
 #include "AnimatedSprite.h"
 #include "Components/RectangleShapeRenderer.h"
 
+// Constructeur de la classe AnimatedSpriteComponent.
+// Initialise les variables membres et cree les sprites et textures.
 AnimatedSpriteComponent::AnimatedSpriteComponent()
     : frameCount(0), frameTime(0.0f), currentFrame(1), currentTime(0.0f), lastState(PlayerSpriteState::Idle)
 {
@@ -8,12 +10,10 @@ AnimatedSpriteComponent::AnimatedSpriteComponent()
     texture = new sf::Texture();
     stateFilePaths[PlayerSpriteState::Idle] = "Assets/Idle.png";
     stateFilePaths[PlayerSpriteState::Running] = "Assets/Run.png";
-    stateFrameCounts[PlayerSpriteState::Idle] = 15;
-    stateFrameCounts[PlayerSpriteState::Running] = 8;
-
 }
 
-
+// Destructeur de la classe AnimatedSpriteComponent.
+// Supprime les sprites et textures.
 AnimatedSpriteComponent::~AnimatedSpriteComponent()
 {
     delete sprite;
@@ -22,17 +22,19 @@ AnimatedSpriteComponent::~AnimatedSpriteComponent()
     texture = nullptr;
 }
 
+// Definit la texture du sprite.
 void AnimatedSpriteComponent::SetTexture(sf::Texture* _texture)
 {
     texture = _texture;
     sprite->setTexture(*texture);
 
-    // Définir l'origine du sprite à son centre
+    // Definir l'origine du sprite a son centre
     defaultOriginX = static_cast<float>(texture->getSize().x / 2.0f);
     defaultOriginY = static_cast<float>(texture->getSize().y / 2.0f);
     sprite->setOrigin(defaultOriginX, defaultOriginY);
 }
 
+// Charge une texture a partir d'un fichier.
 void AnimatedSpriteComponent::LoadTexture(const std::string& texturePath)
 {
     if (!texture->loadFromFile(texturePath))
@@ -42,11 +44,13 @@ void AnimatedSpriteComponent::LoadTexture(const std::string& texturePath)
     sprite->setTexture(*texture);
 }
 
+// Definit le rectangle de texture du sprite.
 void AnimatedSpriteComponent::SetTextureRect(int _left, int _top, int _width, int _height)
 {
     sprite->setTextureRect(sf::IntRect(_left, _top, _width, _height));
 }
 
+// Definit la direction du mouvement du sprite.
 void AnimatedSpriteComponent::SetDirection(MovementDirection _direction)
 {
     direction = _direction;
@@ -63,6 +67,7 @@ void AnimatedSpriteComponent::SetDirection(MovementDirection _direction)
     }
 }
 
+// Recupere le chemin du fichier de texture pour un etat donne.
 std::string AnimatedSpriteComponent::GetStateFilePath(PlayerSpriteState state) const
 {
     if (stateFilePaths.count(state) > 0) {
@@ -74,6 +79,7 @@ std::string AnimatedSpriteComponent::GetStateFilePath(PlayerSpriteState state) c
     }
 }
 
+// Recupere le nombre de frames pour un etat donne.
 int AnimatedSpriteComponent::GetStateFrameCount(PlayerSpriteState state) const
 {
     if (stateFrameCounts.count(state) > 0) {
@@ -85,22 +91,21 @@ int AnimatedSpriteComponent::GetStateFrameCount(PlayerSpriteState state) const
     }
 }
 
-
+// Met a jour le sprite anime.
 void AnimatedSpriteComponent::Update(float deltaTime)
 {
-    // Vérifier si l'état a changé
+    // Verifier si l'etat a change
     if (state != lastState) {
-        // Charger la texture pour l'état actuel
+        // Charger la texture pour l'etat actuel
         LoadTexture(GetStateFilePath(state));
-        // Mettre à jour frameCount pour l'état actuel
+        // Mettre a jour frameCount pour l'etat actuel
         SetFrameCount(GetStateFrameCount(state));
-        // Mettre à jour lastState
+        // Mettre a jour lastState
         lastState = state;
     }
     currentTime += deltaTime;
     if (currentTime >= frameTime)
-    { 
-
+    {
         currentFrame = (currentFrame + 1) % frameCount;
         if (frameCount > 0 && texture != nullptr) {
             sprite->setTextureRect(sf::IntRect(currentFrame * texture->getSize().x / frameCount, 0, texture->getSize().x / frameCount, texture->getSize().y));
@@ -109,12 +114,13 @@ void AnimatedSpriteComponent::Update(float deltaTime)
     }
     GameObject* owner = GetOwner();
     if (owner != nullptr) {
-        // Mettez à jour la position du sprite pour qu'il soit centré sur le gameObject
+        // Mettez a jour la position du sprite pour qu'il soit centre sur le gameObject
         Maths::Vector2f gameObjectSize = owner->GetComponent<RectangleShapeRenderer>()->GetSize();
         sprite->setPosition(owner->GetPosition().x, owner->GetPosition().y);
     }
 }
 
+// Dessine le sprite sur la fenêtre donnee.
 void AnimatedSpriteComponent::Render(sf::RenderWindow* window)
 {
     window->draw(*sprite);
